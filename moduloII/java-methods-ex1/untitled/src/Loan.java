76d538fc63d845b4ce0cc2bb33d3d7a6b0eefed2
juriaCardozo <ccardozojulia@gmail.com>
2022-07-11 12:22:05 -0300

    creating classes

---
+++ moduloII/java-methods-ex1/untitled/src/Loan.java
@@ -0,0 +1,2 @@
+public class Loan {
+}
---
+++ moduloII/java-methods-ex1/untitled/src/Operacoes.java
@@ -0,0 +1,23 @@
+public class Operacoes {
+
+    public static void sum(double a, double b){
+        double result = a + b;
+        System.out.println(a + " + " + b + " = " + result);
+    }
+
+    public static void minus(double a, double b){
+        double result = a - b;
+        System.out.println(a + " - " + b + " = " + result);
+    }
+
+    public static void muiltiply(double a, double b){
+        double result = a * b;
+        System.out.println(a + " * " + b + " = " + result);
+    }
+
+    public static void divide(double a, double b){
+        double result = a / b;
+        System.out.println(a + " / " + b + " = " + result);
+    }
+
+}
