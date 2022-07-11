59f724861328deb516b6f7bf560bd7fe37b0d457
juriaCardozo <ccardozojulia@gmail.com>
2022-07-11 12:39:37 -0300

    mensagem

--- moduloII/java-methods-ex1/untitled/src/Message.java
+++ moduloII/java-methods-ex1/untitled/src/Message.java
@@ -1,3 +1,13 @@
 public class Message {
 
+    public static void greetings(int hour){
+        if(hour >= 5 && hour <= 11){
+            System.out.println("Bom dia!\n");
+        }else if(hour >= 12 && hour <= 17){
+            System.out.println("Boa tarde!\n");
+        }else if(hour >= 18 && hour <= 24 || hour >= 1 && hour <= 4){
+            System.out.println("Boa noite!\n");
+        }
+    }
+
 }
--- moduloII/java-methods-ex1/untitled/src/Program.java
+++ moduloII/java-methods-ex1/untitled/src/Program.java
@@ -4,10 +4,17 @@ public class Program {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Insira a hora: ");
+        int hour = sc.nextInt();
+        Message.greetings(hour);
+
+        ////////////////////////////////////
+
         System.out.println("Calculadora!");
         System.out.println("Insira dois números: ");
-        double a = sc.nextInt();
-        double b = sc.nextInt();
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println("\nOperações: ");
         Operacoes.sum(a, b);
         Operacoes.minus(a, b);
         Operacoes.multiply(a, b);
