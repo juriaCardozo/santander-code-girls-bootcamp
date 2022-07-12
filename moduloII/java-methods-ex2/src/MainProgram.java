5c2d0a43d5a04e0b8d95750314b8e8d37fcaed7a
juriaCardozo <ccardozojulia@gmail.com>
2022-07-12 12:04:12 -0300

    ex2

---
+++ moduloII/java-methods-ex2/src/MainProgram.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira um valor para calcular a área do quadrado: ");
+        double areaQuadrado = sc.nextDouble();
+        Quadrilatero.area(areaQuadrado);
+
+        System.out.println();
+
+        System.out.println("Insira 2 valores para calcular a área do retângulo: ");
+        double l1 = sc.nextDouble();
+        double l2 = sc.nextDouble();
+        Quadrilatero.area(l1, l2);
+
+        System.out.println();
+
+        System.out.println("Insira 3 valores para calcular a área do trapézio: ");
+        double b1 = sc.nextDouble();
+        double b2 = sc.nextDouble();
+        double h = sc.nextDouble();
+        Quadrilatero.area(b1, b2, h);
+    }
+}
---
+++ moduloII/java-methods-ex2/src/Quadrilatero.java
@@ -0,0 +1,13 @@
+public class Quadrilatero {
+    public static void area(double lado) {
+        System.out.println("Área do quadrado = " + lado * lado);
+    }
+
+    public static void area(double lado, double lado2) {
+        System.out.println("Área do Retângulo = " + lado * lado2);
+    }
+
+    public static void area(double bMaior, double bMenor, double altura) {
+        System.out.println("Área do Trapézio = " + (bMaior + bMenor) * altura / 2);
+    }
+}
