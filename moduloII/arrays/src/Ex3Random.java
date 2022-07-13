7988aedf2c6b07ffec1200bafa5129865380ba33
juriaCardozo <ccardozojulia@gmail.com>
2022-07-13 09:13:42 -0300

    ex3 array

---
+++ moduloII/arrays/src/Ex3Random.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ex3Random {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        Random gen = new Random();
+//
+//        int[] array = new int[20];
+//
+//        for(int i = 0; i < args.length; i++){
+//            int number = gen.nextInt(100);
+//            array[i] = number;
+//        }
+//
+//        System.out.println("Números aleatórios gerados: ");
+//        for(int number : array){
+//            System.out.print(number + " ");
+//            System.out.print((number + 1) + " ");
+//        }
+
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for(int i = 0; i < numerosAleatorios.length; i++) {
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+
+        System.out.print("Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print(numero + " ");
+        }
+
+        System.out.print("\nAntecessor dos Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print((numero-1) + " ");
+        }
+
+        System.out.print("\nSucessores dos Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print((numero+1) + " ");
+        }
+
+    }
+}
