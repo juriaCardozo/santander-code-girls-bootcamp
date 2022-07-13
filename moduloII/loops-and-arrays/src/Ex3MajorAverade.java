186006f2045f671ca9b0eca8fbdfab40d147f3ea
juriaCardozo <ccardozojulia@gmail.com>
2022-07-12 21:56:55 -0300

    exercicios

--- moduloII/loops-and-arrays/src/Ex3MajorAverade.java
+++ moduloII/loops-and-arrays/src/Ex3MajorAverade.java
@@ -4,6 +4,22 @@ public class Ex3MajorAverade {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int[] numbers = new int[5];
+        double major = 0;
+        double average = 0;
 
+        for(int i = 0; i < 5; i++){
+            System.out.println("Insira o número " + i + ": ");
+            numbers[i] = sc.nextInt();
+            if(numbers[i] > major){
+                major = numbers[i];
+            }
+            average += numbers[i];
+        }
+
+        average /= 5;
+
+        System.out.println("Média dos números inseridos: " + average);
+        System.out.println("Maior número inserido: " + major);
     }
 }
