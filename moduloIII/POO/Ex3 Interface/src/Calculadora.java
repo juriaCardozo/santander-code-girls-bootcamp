3182e90f33a02f0d2294d9458f5c5ea40c9b323a
juriaCardozo <ccardozojulia@gmail.com>
2022-07-17 13:46:49 -0300

    POO ex3 interface

--- moduloIII/POO/Ex3 Interface/src/Calculadora.java
+++ moduloIII/POO/Ex3 Interface/src/Calculadora.java
@@ -1,2 +1,17 @@
-public class Calculadora {
+public class Calculadora implements OperacaoMatematica{
+    public void soma(double valor1, double valor2){
+        System.out.println("Soma: " + valor1 + valor2);
+    }
+
+    public void subtracao(double valor1, double valor2){
+        System.out.println("Subtração: " + valor1 + valor2);
+    }
+
+    public void multiplicacao(double valor1, double valor2){
+        System.out.println("Multiplicação: " + valor1 + valor2);
+    }
+
+    public void divisao(double valor1, double valor2){
+        System.out.println("Divis: " + valor1 + valor2);
+    }
 }
