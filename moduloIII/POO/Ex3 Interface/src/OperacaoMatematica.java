45088203455c0463dba4584eb99299e097db393c
juriaCardozo <ccardozojulia@gmail.com>
2022-07-17 13:43:44 -0300

    POO ex3 interface

--- moduloIII/POO/Ex3 Interface/src/OperacaoMatematica.java
+++ moduloIII/POO/Ex3 Interface/src/OperacaoMatematica.java
@@ -1,2 +1,6 @@
 public interface OperacaoMatematica {
+    void soma(double valor1, double valor2);
+    void subtracao(double valor1, double valor2);
+    void multiplicacao(double valor1, double valor2);
+    void divisao(double valor1, double valor2);
 }
