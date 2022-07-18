02505204e6b977902ef373d18e047cfde9fabd53
juriaCardozo <ccardozojulia@gmail.com>
2022-07-18 19:10:48 -0300

    classe conteudo

--- moduloIII/POO/desafio-dio-poo/src/br/com/dio/desafio/dominio/Mentoria.java
+++ moduloIII/POO/desafio-dio-poo/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,9 +2,44 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public abstract class Mentoria {
+public class Mentoria {
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
 
-    protected static final double XP_PADRAO = 10d;
+    public Mentoria() {
+    }
 
-    public abstract double calcularXP();
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+    }
 }
