78dad95897dbc19e50e1866d59ac13b297cc7e93
juriaCardozo <ccardozojulia@gmail.com>
2022-07-18 19:12:20 -0300

    classe conteudo

--- moduloIII/POO/desafio-dio-poo/src/br/com/dio/desafio/dominio/Conteudo.java
+++ moduloIII/POO/desafio-dio-poo/src/br/com/dio/desafio/dominio/Conteudo.java
@@ -1,25 +1,11 @@
 package br.com.dio.desafio.dominio;
 
-public class Conteudo {
+public abstract class Conteudo {
+
+    protected static final double XP_PADRAO = 10d;
+
     private String titulo;
     private String descricao;
 
-    public Conteudo() {
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+    public abstract double calcularXP();
 }
