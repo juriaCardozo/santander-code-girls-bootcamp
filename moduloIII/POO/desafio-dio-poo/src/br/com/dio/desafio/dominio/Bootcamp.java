1780253183b31ef6fb904a899314cd24eb7f1046
juriaCardozo <ccardozojulia@gmail.com>
2022-07-19 11:45:10 -0300

    classes dev e bootcamp

--- moduloIII/POO/desafio-dio-poo/src/br/com/dio/desafio/dominio/Bootcamp.java
+++ moduloIII/POO/desafio-dio-poo/src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -1,4 +1,5 @@
 package br.com.dio.desafio.dominio;
 
 public class Bootcamp {
+
 }
--- moduloIII/POO/desafio-dio-poo/src/br/com/dio/desafio/dominio/Dev.java
+++ moduloIII/POO/desafio-dio-poo/src/br/com/dio/desafio/dominio/Dev.java
@@ -1,4 +1,10 @@
 package br.com.dio.desafio.dominio;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Dev {
+    private String nome;
+    private final Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private final Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 }
