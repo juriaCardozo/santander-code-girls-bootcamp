dbafe1e8c72daa04e035707a9536ede1d8ab1953
juriaCardozo <ccardozojulia@gmail.com>
2022-07-24 11:00:29 -0300

    Java exceptions - unchecked

--- moduloIII/java-exceptions/src/UncheckedException.java
+++ moduloIII/java-exceptions/src/UncheckedException.java
@@ -2,20 +2,27 @@ import javax.swing.*;
 
 public class UncheckedException {
     public static void main(String[] args) {
-        String a = JOptionPane.showInputDialog("Numerador: ");
-        String b = JOptionPane.showInputDialog("Denominador: ");
 
-        try {
-            int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
-            System.out.println("Resultado: " + resultado);
-        }catch (NumberFormatException e){
-            JOptionPane.showMessageDialog(null, "Entrada inválida, informe um número inteiro! " + e.getMessage());
-            //e.printStackTrace();
-        } catch (ArithmeticException e){
-            JOptionPane.showMessageDialog(null, "Impossível divisão por 0! " + e.getMessage());
-        } finally {
-            System.out.println("Finally!");
-        }
+        boolean continueLooping = true;
+        do {
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador: ");
+
+            try{
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+                System.out.println("Resultado: " + resultado);
+                continueLooping = false;
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Entrada inválida, informe um número inteiro! " + e.getMessage());
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Impossível dividir um número por 0.");
+            }
+            finally {
+                System.out.println("Chegou no finally!");
+            }
+        } while(continueLooping);
 
 
         System.out.println("Segue aqui ainda...");
